23e8df662afb33e00e6ee50fc944b6538f09b983
Justin Turney <justin.turney@gmail.com>
2020-02-28 15:25:03 -0500

    Adds updated input.py

--- 2020/programming/input.py
+++ 2020/programming/input.py
@@ -1,6 +1,6 @@
 Settings = dict()
 
-Settings["basis"] = "STO-3G"
+Settings["basis"] = "6-31G"
 Settings["molecule"] = """
   0 1
   O
@@ -16,4 +16,14 @@ Settings["scf_max_iter"] = 50
 
 # 'o' frozen doubly occupied orbitals
 # 'a' active orbitals
-Settings["active_space"] = 'ooooaaa'  # Any remaining that remain refer to frozen unoccupied orbitals
+# 'u' frozen unoccupied orbitals
+#       If insufficient u's are provided, you're code will append additional 
+#       u's to get the length to the number of molecular orbitals
+# 'full' automatically set the active space to 'a' * nmo
+Settings["active_space"] = 'oaaaaaa'
+
+# If 'full' perform a full CI
+# If an integer then is the excitation level
+#    (1 = CIS, 2 = CISD, 3 = CISDT, etc.)
+#Settings["excitation_level"] = 2
+Settings["excitation_level"] = 'full' 
