edfb5f213747c3821360b006f85516445b0e9091
Justin Turney <justin.turney@gmail.com>
2020-04-10 08:11:59 -0400

    Adds DF-MP2 handout

--- 2020/programming/input.py
+++ 2020/programming/input.py
@@ -1,6 +1,6 @@
 Settings = dict()
-
-Settings["basis"] = "6-31G"
+Settings["basis"] = "STO-3G"
+Settings["df_basis"] = "STO-3G"
 Settings["molecule"] = """
   0 1
   O
@@ -13,17 +13,15 @@ Settings["molecule"] = """
 Settings["nalpha"] = 5
 Settings["nbeta"] = 5
 Settings["scf_max_iter"] = 50
-
 # 'o' frozen doubly occupied orbitals
 # 'a' active orbitals
 # 'u' frozen unoccupied orbitals
-#       If insufficient u's are provided, you're code will append additional 
-#       u's to get the length to the number of molecular orbitals
+#    If insufficient u's are provided append additional u's to get the 
+#    length to the number of molecular orbitals
 # 'full' automatically set the active space to 'a' * nmo
-Settings["active_space"] = 'oaaaaaa'
-
+Settings["active_space"] = 'oooaaaa'
+#Settings["active_space"] = 'full'
 # If 'full' perform a full CI
 # If an integer then is the excitation level
 #    (1 = CIS, 2 = CISD, 3 = CISDT, etc.)
-#Settings["excitation_level"] = 2
 Settings["excitation_level"] = 'full' 
